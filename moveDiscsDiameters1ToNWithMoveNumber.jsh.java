0feaefb4bf249882fa8cc18ec48ad1710e66e4b5
Stanley Oberio <oberio.stanley@gmail.com>
2024-03-07 14:54:23 +0800

    moveDiscsDiameters1ToNWithMoveNumber.jsh.java

---
+++ moveDiscsDiameters1ToNWithMoveNumber.jsh.java
@@ -0,0 +1,20 @@
+void moveDiscsDiameters1ToN(int n, String startPeg,
+        String sparePeg, String endPeg, BigInteger[] moveNumber) {
+    if (n == 1) {
+        System.out.printf("%d Move top disc from %s to top of %s.%n",
+                moveNumber[0] = moveNumber[0].add(BigInteger.ONE), startPeg,
+                endPeg);
+    } else {
+        var nMinus1 = n;
+        nMinus1--;
+        moveDiscsDiameters1ToN(nMinus1, startPeg, endPeg, sparePeg,
+                moveNumber);
+        System.out.printf("%d Move top disc from %s to top of %s.%n",
+                moveNumber[0] = moveNumber[0].add(BigInteger.ONE), startPeg,
+                endPeg);
+        moveDiscsDiameters1ToN(nMinus1, sparePeg, startPeg, endPeg,
+                moveNumber);
+    }
+}
+moveDiscsDiameters1ToN(3, "middle peg", "right peg", "left peg",
+        new BigInteger[]{BigInteger.ZERO});
